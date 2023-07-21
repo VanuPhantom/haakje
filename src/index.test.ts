098235fa7d660e58e79703d17a2ce66608cbeebc
Tess Ellenoir Duursma <tess.e.duursma@gmail.com>
2023-07-21 15:25:12 +0200

    Make the timer tests more reliable

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,27 @@
 import { act, renderHook } from "@testing-library/react";
+import { DateTime, Settings } from "luxon";
 import { useTime } from ".";
 import { sleep } from "./utils/time";
 
 test("useTime ticks", async () => {
+  let now = 0;
+  Settings.now = () => now;
+  await sleep(1000);
+
   const { result } = renderHook(() => useTime("second"));
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     const startTime = result.current.toSeconds();
 
-    await act(() => sleep(1000));
+    await act(async () => {
+      console.info(DateTime.now().toSeconds());
+      now += 1000;
+      await sleep(1000);
+      console.info(DateTime.now().toSeconds());
+    });
 
     const endTime = result.current.toSeconds();
 
     expect(endTime - startTime).toBe(1);
   }
-}, 10500);
+}, 8000);
